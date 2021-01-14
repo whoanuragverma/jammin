f84cfbe9128bb1704325049adc6d4652e5185d04
Anurag Verma <anu.anuragverma@gmail.com>
2021-01-14 16:38:00 +0530

    Fix Redirect Link

--- src/app/auth/invite/invite.component.ts
+++ src/app/auth/invite/invite.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'invite',

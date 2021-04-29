c23fb64df6b74a2031f2affb367424c50ee593d5
Anurag Verma <anu.anuragverma@gmail.com>
2021-04-29 14:52:41 +0530

    FIX

--- src/app/components/api.service.ts
+++ src/app/components/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiService {
-  private base = 'https://api.jammin.ml';
+  private base = 'https://api.jammin.workers.dev';
   constructor(private http: HttpClient) {}
   autoComplete(query: string) {
     return this.http.get(`${this.base}/autocomplete?query=${query}`);
